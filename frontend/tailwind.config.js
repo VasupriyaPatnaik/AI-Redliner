fe86e7c962dfa52c2d0333f25eed1097b8c891a7
B. Vasupriya Patnaik <vasupriyab004@gmail.com>
2025-06-09 17:31:59 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
         serif: ['Libre Baskerville', 'Georgia', 'serif'],
       },
       colors: {
